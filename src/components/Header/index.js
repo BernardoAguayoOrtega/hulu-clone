631efaa8b6717c38bd83da73e37a9a48b31697ee
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-21 11:49:33 -0500

    install material ui components and icons instead styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,7 @@
 //import React
 import React from 'react';
-//import header content
-import { HeaderContent } from './styles';
 
 //create and export header component
 export const Header = () => {
-	return <HeaderContent>hey</HeaderContent>;
+	return <header className={'header'}>hey</header>;
 };
--- src/components/Header/styles.js
+++
@@ -1,5 +0,0 @@
-//import styled component
-import styled from 'styled-components';
-
-//create and export header styled component
-export const HeaderContent = styled.header``;
