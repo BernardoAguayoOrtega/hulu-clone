c21f5e2970df54c7918bfcc3ff68bc2055dc4741
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-21 11:34:35 -0500

    instal stylted components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,12 @@
 //import react
 import React from 'react';
+//import header component
 
 //create and export app component
-export const App = () => <h1>App</h1>;
+export const App = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
