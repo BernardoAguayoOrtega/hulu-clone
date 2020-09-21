4ae0242107528e4913f1f59550e16b459991735a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-21 12:28:06 -0500

    fix material ui icons problem

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,19 @@
 //import React
 import React from 'react';
+//import icons
+import HomeIcon from '@material-ui/icons/Home';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutLineIcon from '@material-ui/icons/PersonOutline';
 
 //create and export header component
 export const Header = () => {
-	return <header className={'header'}>hey</header>;
+	return (
+		<header className={'header'}>
+			{'the icons of the header'}
+			<div className='header__icons'></div>{' '}
+		</header>
+	);
 };
